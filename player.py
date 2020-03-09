acf8333c9303e34e4841f06eca3edfac0aceffba
B. Haracewiat <b.haracewiat@gmail.com>
2020-03-09 22:37:18 +0100

    Code base
    Added basic classes to build the program on.
    
    To do:
    - spawn a player every time a client connects

---
+++ player.py
@@ -0,0 +1,24 @@
+import pygame
+
+
+class Player():
+    width = height = 50
+
+    def __init__(self, startx, starty, color=(84, 165, 236)):
+        self.x = startx
+        self.y = starty
+        self.velocity = 4
+        self.color = color
+
+    def draw(self, g):
+        pygame.draw.circle(g, self.color, (self.x, self.y), self.width)
+
+    def move(self, key):
+        if key == 0:
+            self.x += self.velocity
+        elif key == 1:
+            self.x -= self.velocity
+        elif key == 2:
+            self.y -= self.velocity
+        else:
+            self.y += self.velocity
